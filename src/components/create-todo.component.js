0ceddd4f8f8fc86365208a0a097e8ef7e919147c
shorpee <mosopewilliamson@gmail.com>
2020-03-23 20:53:38 +0100

    added methods to update state properties

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -8,4 +8,33 @@ export default class CreateTodo extends Component {
             </div>
         )
     }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todo_description: '',
+            todo_responsible: '',
+            todo_priority: '',
+            todo_completed: false
+        }
+    }
+
+    onChangeTodoDescription(e) {
+        this.setState({
+            todo_description: e.target.value
+        });
+    }
+
+    onChangeTodoResponsible(e) {
+        this.setState({
+            todo_responsible: e.target.value
+        });
+    }
+
+    onChangeTodoPriority(e) {
+        this.setState({
+            todo_priority: e.target.value
+        });
+    }
 }
