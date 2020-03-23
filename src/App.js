e19f77f4e2dbd6a051c4dab04b60eaed526c9538
shorpee <mosopewilliamson@gmail.com>
2020-03-23 20:00:00 +0100

    adding styling

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
+
+import logo from "./logo.png";
+
 class App extends Component {
   render () {
   return (
     <Router>
-      <Route path="/" exact component={TodosList} />
-      <Route path="/edit/:id" component={EditTodo} />
-      <Route path="/create" component={CreateTodo} />
       <div className="container">
-        <h2>MERN-Stack Todo App</h2>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href=""></a>
+        </nav>
+        <Route path="/" exact component={TodosList} />
+        <Route path="/edit/:id" component={EditTodo} />
+        <Route path="/create" component={CreateTodo} />
       </div>
-    </Router>
+      </Router>
     );
   }
 }
