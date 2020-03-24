8ac6fdd998fb47788636c38222b6ef1b54660b14
shorpee <mosopewilliamson@gmail.com>
2020-03-24 04:45:55 +0100

    add submit event

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -37,4 +37,20 @@ export default class CreateTodo extends Component {
             todo_priority: e.target.value
         });
     }
+    
+    onSubmit(e) {
+        e.preventDefault();
+
+        console.log(`Form submitted:`);
+        console.log(`Todo Description: ${this.state.todo_description}`);
+        console.log(`Todo Responsible: ${this.state.todo_responsible}`);
+        console.log(`Todo Priority: ${this.state.todo_priority}`);
+
+        this.setState({
+            todo_description: '',
+            todo_responsible: '',
+            todo_priority: '',
+            todo_completed: false
+        })
+    }
 }
